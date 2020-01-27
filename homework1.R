0091d3fe69e45d17ce127d91b092c1f83e1c4fb9
lunaryuu <60085327+lunaryuu@users.noreply.github.com>
2020-01-27 00:03:35 -0500

    Delete homework1.R

--- homework1.R
+++
@@ -1,8 +0,0 @@
-assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
-myMean <- function(assignment2) 
-  { 
-    return(sum(assignment2)/length(someData)) 
-  }
-myMean()
-
-# The function does not work because the 
