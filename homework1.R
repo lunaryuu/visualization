4cefb47a542dca7907a8917f08554c49a8bac089
lunaryuu <60085327+lunaryuu@users.noreply.github.com>
2020-01-27 00:00:44 -0500

    Add files via upload

---
+++ homework1.R
@@ -0,0 +1,8 @@
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+myMean <- function(assignment2) 
+  { 
+    return(sum(assignment2)/length(someData)) 
+  }
+myMean()
+
+# The function does not work because the 
